01b8289130e1a40b9b51938293c1d4c530d7d24d
Руслан Заликов <ruslanzalikov@MacBook-Pro-Ruslan.local>
2021-11-29 18:29:58 +0300

    First commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget,
+QLineEdit, QHBoxLayout, QVBoxLayout, QPushButton
+
+class Calculator(QWidget):
+ def __init__(self):
+ super(Calculator, self).__init__()
